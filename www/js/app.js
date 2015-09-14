b8683654f4b5e200f3ff73809fdbc3394dd51173
Ken <ken@sipantic.net>
2015-09-14 08:11:29 -0400

    Splashscreen

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic','ionic.service.core','ionic.service.analytics', 'starter.controllers', 'starter.directives'])
 
@@ -16,7 +17,7 @@ angular.module('starter', ['ionic','ionic.service.core','ionic.service.analytics
         // 204072158929 Google Project number. 
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
 
@@ -29,8 +30,13 @@ angular.module('starter', ['ionic','ionic.service.core','ionic.service.analytics
 })
 
 .config(function ($stateProvider, $urlRouterProvider) {
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
     $stateProvider
 
+  // setup an abstract state for the app directive
       .state('app', {
           url: '/app',
           abstract: true,
